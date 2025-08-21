ad7d0503a12851d1e2a3df9921e1adc694cdc23d
Nivis <barzie5554@gmail.com>
2025-08-21 20:19:12 +0200

    Added VeterinariansPage

---
+++ Pages/VeterinariansPage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using PetClinicTestAutomation.Base;
+
+
+namespace PetClinicTestAutomation.Pages;
+
+
+public class VeterinariansPage
+{
+    private readonly ElementInteractions elementInteractions;
+
+    #region Locators
+    private readonly string btnVeterinariansLocator = "//a[@title='veterinarians']";
+    private readonly string lstVeterinariansLocator = "//table[@id='vets']";
+    //TODO: Rest of the locators for at least one test
+
+    #endregion
+    
+    //TODO: Methods
+    
+}
